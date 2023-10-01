a8781425fee9005ee176b72bcb04f123a3a20a67
lewis <lcb005@uark.edu>
2023-09-30 21:53:03 -0500

    removed comment

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ const createWindow = () => {
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
-            // nodeIntegration: true
         },
     })
 
